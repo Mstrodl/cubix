4a65209532d16f9880441d3952f7df39d5c0f990
Lukas Mendes <lkmnds@gmail.com>
2016-01-25 13:24:50 -0300

    Close #7
     * added mouse device at /dev/mouse

---
+++ lib/devices/mouse_device.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env lua
+--mouse device
+
+dev_mouse = {}
+dev_mouse.name = '/dev/mouse'
+dev_mouse.device = {}
+dev_mouse.device.device_read = function(bytes)
+    local event, button, x, y = os.pullEvent("mouse_click")
+    return x..':'..y..':'..button
+end
+
+dev_mouse.device.device_write = function(s)
+    ferror("devmouse: cant write to mouse device")
+    os.sys_signal(os.signals.SIGILL)
+    return 1
+end
