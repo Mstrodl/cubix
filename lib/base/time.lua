2e42c9d42607284902868e86811a6cf361f9e8a4
Luna Mendes <lkmnds@gmail.com>
2016-09-20 11:23:42 -0300

    layout time.lua

--- lib/base/time.lua
+++ lib/base/time.lua
@@ -3,5 +3,15 @@
     time.lua - manages time functions and /dev/time
 ]]
 
+TimeDevice = class(Device, function(self)
+end)
+
+function TimeDevice:read(bytes)
+    return getTime_fmt()
+end
+
 function libroutine()
+    if udev then
+        udev.add_device("/dev/time", TimeDevice())
+    end
 end
